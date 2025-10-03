37e32272341044e65f6e18f864aee364269df1d0
Dhanush <dhanush1004raj@gmail.com>
2025-10-03 09:29:52 +0530

    remove rough/

--- rough/rough.py
+++
@@ -1,7 +0,0 @@
-from src.main import GmailAssistant
-from agno.utils.pprint import pprint_run_response
-
-agent = GmailAssistant.setup_gmail_agent()
-
-response = GmailAssistant.perform_task(agent=agent, task="Give me the summary of my latest email in under 50 words")
-print(response)
